8be274b4bb6940da67a966693bec48ca35e6b946
Flávio Marques <flaviomarques2@hotmail.com>
2023-07-26 22:13:42 -0300

    estudando lógica de radiobutton

---
+++ lib/radio.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RadioExemplo extends StatefulWidget {
+  const RadioExemplo({super.key});
+
+  @override
+  State<RadioExemplo> createState() => _RadioExemploState();
+}
+
+class _RadioExemploState extends State<RadioExemplo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+        Radio(value: 1, groupValue: 1, onChanged: null)
+
+      ],));}}
